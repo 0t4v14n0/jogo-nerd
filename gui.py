38170c3777bb4eca3f0cc4a099ece3031d3d675a
Otaviano Cruz Gouveia de Arruda Neto <58171774+0t4v14n0@users.noreply.github.com>
2024-03-30 00:08:42 -0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ from tkinter import PhotoImage
 from models.calcular import Calcular
 
 dificuldade_global = 0
+pontos = 0
 
 class MinhaApp(tk.Tk):
     def __init__(self):
@@ -37,8 +38,8 @@ class PaginaInicial(tk.Frame):
         self.labelfundo = tk.Label(self, image=self.img_menup)
         self.labelfundo.pack()
 
-        submit_button = tk.Button(self, text="INICIAR", command=lambda: controller.mostrar_pagina(Dificuldade))
-        submit_button.place(x=200, y=400,width=100,height=20)
+        submit_button = tk.Button(self, text="INICIAR", command=lambda: controller.mostrar_pagina(Dificuldade),font=("Arial", 30))
+        submit_button.place(x=160, y=400)
 
 class Dificuldade(tk.Frame):
     def __init__(self, parent, controller):
@@ -77,20 +78,20 @@ class Dificuldade(tk.Frame):
 
         var = tk.IntVar()
 
-        self.radio1 = tk.Radiobutton(self, text="Fácil", variable=var, value=1)
-        self.radio1.place(x=100, y=300,width=100,height=20)
+        self.radio1 = tk.Radiobutton(self, text="Fácil", variable=var, value=1,font=("Arial", 20))
+        self.radio1.place(x=50, y=250)
 
-        self.radio2 = tk.Radiobutton(self, text="Médio", variable=var, value=2)
-        self.radio2.place(x=100, y=350,width=100,height=20)
+        self.radio2 = tk.Radiobutton(self, text="Médio", variable=var, value=2,font=("Arial", 20))
+        self.radio2.place(x=50, y=300)
 
-        self.radio3 = tk.Radiobutton(self, text="Difícil", variable=var, value=3)
-        self.radio3.place(x=100, y=400,width=100,height=20)
+        self.radio3 = tk.Radiobutton(self, text="Difícil", variable=var, value=3,font=("Arial", 20))
+        self.radio3.place(x=50, y=350)
 
-        self.radio3 = tk.Radiobutton(self, text="Nerd", variable=var, value=4)
-        self.radio3.place(x=100, y=450,width=100,height=20)
+        self.radio3 = tk.Radiobutton(self, text="Nerd", variable=var, value=4,font=("Arial", 20))
+        self.radio3.place(x=50, y=400)
 
-        self.selecionar_button = tk.Button(self, text="Selecionar", command=mostrar_selecao)
-        self.selecionar_button.place(x=300, y=400,width=100,height=20)
+        self.selecionar_button = tk.Button(self, text="Selecionar", command=mostrar_selecao,font=("Arial", 20))
+        self.selecionar_button.place(x=280, y=400)
 
 class Game(tk.Frame):
     def __init__(self, parent, controller):
@@ -98,8 +99,44 @@ class Game(tk.Frame):
 
         self.controller = controller
 
+        self.img_menup = PhotoImage(file="imagens/fnadaa.png")
+
+        self.labelfundo = tk.Label(self, image=self.img_menup)
+        self.labelfundo.pack()
+
+        def atuponto():
+            newponto = "Voce tem "+ str(pontos) +" pontos"
+            self.texto_dinamico = tk.Label(self, text= newponto, font=("Arial", 15))
+            self.texto_dinamico.place(x=300, y=10)
+            return
+        
+        def enviar_resu():
+            global pontos 
+            resposta = self.entrada_texto.get()
+            if calc.checar_resultado(int(resposta)):
+               pontos = pontos + 1
+               atuponto()
+            print("enviado:", resposta)
+
         calc: Calcular = Calcular(dificuldade_global)
         print (calc)
+
+        atuponto()
+
+        self.texto_dinamico = tk.Label(self, text=calc.mostrar_operacao(), font=("Arial", 30))
+        self.texto_dinamico.place(x=50, y=300)
+
+        self.entrada_texto = tk.Entry(self,font=("Arial", 30))
+        self.entrada_texto.place(x=250, y=300,width=150)
+
+        self.botao_enviar = tk.Button(self, text="Enviar", command=enviar_resu,font=("Arial", 20))
+        self.botao_enviar.place(x=100, y=350)
+
+class Continuar(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+
+        self.controller = controller
 
 if __name__ == "__main__":
     app = MinhaApp()
